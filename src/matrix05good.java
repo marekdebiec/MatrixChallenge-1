285cd2b69b50c5c2ff4331ccfb640141c3f43cd4
Marek <marekdebiec@gmail.com>
2023-07-26 19:30:41 +0200

    StringChallenge

--- src/matrix05good.java
+++ src/matrix05good.java
@@ -1,4 +1,20 @@
+/*Matrix Challenge
+        Have the function MatrixChallenge(strArr) take the strArr parameter being passed which will be a 2D matrix
+        of 0 and 1's, and determine the area of the largest rectangular submatrix that contains all 1's.
+        For example: if strArr is ["10100", "10111", "11111", "10010"] then this looks like the following matrix:
 
+        1 0 1 0 0
+        1 0 1 1 1
+        1 1 1 1 1
+        1 0 0 1 0
+
+        For the input above, you can see the bolded 1's create the largest rectangular submatrix of size 2x3,
+        so your program should return the area which is 6. You can assume the input will not be empty.
+        Examples
+        Input: new String[] {"1011", "0011", "0111", "1111"}
+        Output: 8
+        Input: new String[] {"101", "111", "001"}
+        Output: 3*/
 import java.util.*;
 import java.io.*;
 
